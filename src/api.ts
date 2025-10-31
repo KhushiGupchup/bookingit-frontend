e55406357f16d1d2f2b83a3140519394fa67d871
KhushiGupchup <khushigupchup@gmail.com>
2025-10-31 15:33:59 +0530

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -2,8 +2,9 @@ import axios from "axios";
 
 // Change baseURL if backend runs elsewhere (e.g., Render)
 const API = axios.create({
-  baseURL: "https://bookit-backend-fas6.onrender.com/",
+  baseURL: "https://bookit-backend-1-xhzr.onrender.com/",
 });
 
 export default API;
+
 
